acfc46cc311bcce12e5ce2863c5507d317e7d6b5
pya1097 <pandurk@ncsu.edu>
2024-01-13 11:34:48 -0500

    initial file structure

---
+++ config.py
@@ -0,0 +1,16 @@
+"""
+gate: guess, assess, try, expand
+(c) 2023, Tim Menzies, BSD-2
+Learn a little, guess a lot, try the strangest guess, repeat
+
+USAGE:
+  python3 gate.lua [OPTIONS] 
+
+OPTIONS:
+  -c --cohen  small effect size               = .35
+  -f --file   csv data file name              = '../data/diabetes.csv'
+  -h --help   show help                       = False
+  -k --k      low class frequency kludge      = 1
+  -m --m      low attribute frequency kludge  = 2
+  -s --seed   random number seed              = 31210
+  -t --todo   start up action                 = 'help' """
