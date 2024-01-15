1ed29903aa8bafc023d6c5b86ee007155004fbe4
Atharva Deo <deovibhav@gmail.com>
2024-01-15 14:06:23 -0500

    Main

---
+++ hw/w2/src/main.py
@@ -0,0 +1,13 @@
+from data import DATA 
+from utils import *
+import csv
+
+
+if __name__ == "__main__":
+    file_path = 'w2/data/auto93.csv'
+    data = DATA(file_path)
+    stats = stats(data)
+
+    with open('/w2/w2/out', 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow([str(stats)])
