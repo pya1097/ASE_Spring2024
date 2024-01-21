f52f962b2866fb10cff482a040a7e9716dbbba1c
pya1097 <pandurk@ncsu.edu>
2024-01-21 14:29:37 -0500

    Added task 2 likelihood functions in NUM, SYM, ROW

--- hw/w3/src/row.py
+++ hw/w3/src/row.py
@@ -1,3 +1,4 @@
+import math
 
 class ROW:
     # Initializing ROW instance
@@ -12,3 +13,16 @@ class ROW:
             n = n + 1
             d = d + abs(col.heaven - col.norm(self.cells([col.at])))**2
         return (d**0.5)/(n**0.5)
+
+    #Finding out how much a row likes the data
+    def like(self, data, n, nHypotheses, the):
+        prior = (len(data.rows) + the.k) / (n + the.k * nHypotheses)
+        out = math.log(prior)
+
+        for col in data.cols.x:
+            v = self.cells[col.at]
+            if v != "?":
+                inc = col.like(v, prior)
+                out += math.log(inc)
+
+        return math.exp(1) ** out
