75ec53d5aa5446935919c482acd2439fb9147ecf
shafa112 <shafa.hassan112@gmail.com>
2024-02-19 20:04:39 -0500

    week 6 and 7 tasks

--- hw/w5/src/main.py
+++ hw/w5/src/main.py
@@ -14,13 +14,34 @@ if __name__ == "__main__":
         row_one = data.rows[0]
         rows_sort = row_one.neighbors(data=data)
         row_num = 1
-        print("Task 1:")
-        print()
-        for row in rows_sort:
-            if(row_num % 30 == 1):
-                print(row_num, row.cells, roundoff(row_one.dist(row, data)))    
-            row_num += 1
+        # print("Task 1:")
+        # print()
+        # for row in rows_sort:
+        #     if(row_num % 30 == 1):
+        #         print(row_num, row.cells, roundoff(row_one.dist(row, data)))    
+        #     row_num += 1
     
 
     data_new = DATA(the['file'])
-    DATA.far(the, data_new)
+    #DATA.far(the, data_new)
+
+    print("Task 1: Implementing the recursive tree\n")
+    t, evals = data_new.tree(True)
+    t.show()
+    print("evals: ", evals)
+    print()
+
+    print("Task 2: Optimization output - Single Descent\n")
+    best, rest, evals = data_new.branch()
+    print("centroid of output cluster: ")
+    print(o(best.mid().cells), o(rest.mid().cells))
+    print("evals: ", evals)
+    print()
+
+    print("Task 3: Double tap\n")
+    best1, rest, evals1 = data_new.branch(32)
+    best2, _, evals2 = best1.branch(4)
+    print("median and best found in that four: ")
+    print(o(best2.mid().cells), o(rest.mid().cells))
+    print("evals: ",evals1 + evals2)
+
